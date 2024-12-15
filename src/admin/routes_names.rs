ebcbeff58a5a6a210a6aaafe29dc9e0e9a451202
vrittech-manoj <manoj@vrittechnologies.com>
2024-12-15 12:47:55 +0545

    fixed

--- src/admin/routes_names.rs
+++ src/admin/routes_names.rs
@@ -9,6 +9,8 @@ impl RouteNames {
         let mut routes = HashMap::new();
         routes.insert("admin_login", "/admin/login");
         routes.insert("admin_dashboard", "/admin/dashboard");
+        routes.insert("get-tools", "/tools/get-tools");
+        routes.insert("add-tools","/tools/add-tools/");
         Self { routes }
     }
 
