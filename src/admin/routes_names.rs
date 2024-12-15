96341f0fdbaeef11a1aae5743d94ef1f534b7312
vrittech-manoj <manoj@vrittechnologies.com>
2024-12-15 15:39:14 +0545

    admin panel ongoing

--- src/admin/routes_names.rs
+++ src/admin/routes_names.rs
@@ -7,7 +7,7 @@ pub struct RouteNames {
 impl RouteNames {
     pub fn new() -> Self {
         let mut routes = HashMap::new();
-        routes.insert("admin_login", "/admin/login");
+        routes.insert("admin_login", "/admin/login/");
         routes.insert("admin_dashboard", "/admin/dashboard/");
         routes.insert("get-tools", "/tools/get-tools");
         routes.insert("add-tools","/tools/add-tools/");
