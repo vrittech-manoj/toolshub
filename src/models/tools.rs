191ba56371444d57689ba0c9343b88e70cdbf6a1
unknown <manoj@vrittechnologies.com>
2024-12-28 23:20:11 +0545

    rust database tools

--- src/models/tools.rs
+++ src/models/tools.rs
@@ -3,7 +3,7 @@ use sea_orm::entity::prelude::*;
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, PartialEq, DeriveEntityModel, Serialize, Deserialize)]
-#[sea_orm(table_name = "users")]
+#[sea_orm(table_name = "tools")]
 pub struct Model {
     #[sea_orm(primary_key)]
     pub id: i32,
