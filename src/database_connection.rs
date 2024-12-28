92b4a10bce4b9fad135869357a4bd5c118168eb8
unknown <manoj@vrittechnologies.com>
2024-12-28 22:49:48 +0545

    rust database

--- src/database_connection.rs
+++ src/database_connection.rs
@@ -1,21 +1,21 @@
-use std::env;
+// use std::env;
 
-use sea_orm::{Database, DatabaseConnection, DbErr};
+// use sea_orm::{Database, DatabaseConnection, DbErr};
 
-use sea_orm::ConnectOptions;
-use std::time::Duration;
-use tracing::log::LevelFilter;
-use color_eyre::Result;
+// use sea_orm::ConnectOptions;
+// use std::time::Duration;
+// use tracing::log::LevelFilter;
+// use color_eyre::Result;
 
 
-/// Function to create the database connection pool
+// /// Function to create the database connection pool
 pub async fn create_db_pool() -> Result<DatabaseConnection, DbErr> {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     
     let mut opt = ConnectOptions::new(database_url);
     opt.max_connections(100)
         .min_connections(5)
-        .connect_timeout(Duration::from_secs(8))
+        .connect_timeout(Duration::from_secs(8))z
         .acquire_timeout(Duration::from_secs(8))
         .idle_timeout(Duration::from_secs(8))
         .max_lifetime(Duration::from_secs(8))
@@ -26,5 +26,5 @@ pub async fn create_db_pool() -> Result<DatabaseConnection, DbErr> {
 }
 
 
-// export DATABASE_URL=postgres://postgres:md@localhost/toolshub
-// cargo run
+// // export DATABASE_URL=postgres://postgres:md@localhost/toolshub
+// // cargo run
